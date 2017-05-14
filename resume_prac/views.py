036324fa31ff79b1e57b25654cb74ef3f7d79ef8
JuneBuug <junee613@gmail.com>
2017-05-14 22:01:52 +0900

    버튼이 submit으로 넘어가지않게함

--- resume_prac/views.py
+++ resume_prac/views.py
@@ -3,6 +3,7 @@ from .models import Person
 from .models import Skill
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+import os
 
 # Create your views here.
 def resume_list(request):
@@ -10,16 +11,33 @@ def resume_list(request):
     return render(request,'resume_prac/resume_list.html',{'resume_list':resume_list})
 
 def resume_write(request):
+    # UPLOAD_DIR = '%Y/%m/%d/orig'
 
     if request.method == 'POST':
         name = request.POST['name']
         position = request.POST['position']
-        # image = request.POST['profile_image']
-        skill = request.POST['skill']
-        degree = request.POST['skill_degree']
         person = Person.objects.create(person_name=name,position=position)
         person.save()
-        skill = Skill.objects.create(devstack=skill,person= person,degree=degree)
+        # image = request.POST['profile_image']
+        # if 'file' in request.FILES:
+        #     file = request.FILES['file']
+        #     filename = file._name
+        #
+        #     fp = open('%s/%s' % (UPLOAD_DIR, filename) , 'wb')
+        #     for chunk in file.chunks():
+        #         fp.write(chunk)
+        #         setattr(person,profile_image,chunk)
+        #         person.save()
+        #     fp.close()
+
+        sk = request.POST['skill']
+        degree = request.POST['skill_degree']
+        # for s in sk:
+        #     d= degree[forloop.counter]
+        #     skill = Skill.objects.create(devstack=s,person= person,degree=d)
+        #     skill.save()
+
+        skill = Skill.objects.create(devstack=sk,person= person,degree=degree)
         skill.save()
         # return HttpResponseRedirect(reverse()'/resume/'+str(person.id))
         return HttpResponseRedirect(reverse('resume:detail', args=(person.id,)))
