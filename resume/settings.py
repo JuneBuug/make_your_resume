a088adc8ea4af7359c18ea3a7182b93d87e5f09b
JuneBuug <junee613@gmail.com>
2017-05-20 00:00:00 +0900

    타임라인 추가

--- resume/settings.py
+++ resume/settings.py
@@ -120,5 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
--- resume/urls.py
+++ resume/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static('uploads', document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
